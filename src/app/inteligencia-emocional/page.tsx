cd1a1eaa5f5d0d0d78661847e3cbfcacd6fcf814
vnxcius <vsgh-gouveia@hotmail.com>
2024-07-04 18:22:10 -0300

    style: redesign inteligencia-emocional page

--- src/app/inteligencia-emocional/page.tsx
+++ src/app/inteligencia-emocional/page.tsx
@@ -1,36 +1,27 @@
 'use client';
-import { ArrowLeft } from "lucide-react";
 import Anchor from "@/components/anchor";
+import Header from "@/components/header";
 
 export default function InteligenciaEmocional() {
   return (
     <>
-      <div className="w-full bg-blue-800 py-5 px-5">
-        <h1 className="font-medium text-white">Autoavaliação de Soft Skills</h1>
-      </div>
+      <Header page="Inteligência Emocional" />
 
-      <div className="max-w-xs mx-auto my-14 space-y-5 text-center">
-        <h1 className="text-xl">Autoavaliação:<br />Inteligência Emocional</h1>
+      <div className="max-w-xs mx-auto my-14 space-y-10 text-xl leading-6">
         <p>
-          A seguir serão apresentados 10 situações presentes no nosso dia a dia.
-          Leia cada uma com atenção e marque a opção que melhor descreve o seu comportamentos nessas situações.
+          Essa <span className="font-semibold text-blue-800">Autoavaliação</span> levará menos de 5 minutos
+        </p>
+        <p>
+          Serão apresentadas 10 situações. Leia com atenção e marque a
+          alternativa que melhor descreve seu comportamento.
         </p>
         <p>
           Suas respostas serão mantidas em sigilo.
         </p>
-        <p>
-          Vamos começar?
-        </p>
 
-        <a href="/results" className="text-xs text-blue-500">TEMP: ir para pág. resultados</a>
       </div>
-      <Anchor title="Começar" link="/formularios" className="my-20 w-fit mx-auto block" />
-      <div className="max-w-xs mx-auto mt-32">
-        <a href="/" className="flex items-center gap-2 text-sm w-fit">
-          <ArrowLeft width={20} />
-          <span>Menu Principal</span>
-        </a>
-      </div>
+
+      <Anchor title="Começar" link="/formularios" className="w-full text-center mx-auto block my-20" />
     </>
   )
 }
