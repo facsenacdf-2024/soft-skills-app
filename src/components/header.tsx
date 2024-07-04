805ca26485be060f66f6e0fb8d94534246f25b1f
vnxcius <vsgh-gouveia@hotmail.com>
2024-07-04 18:01:50 -0300

    style: stylize header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,19 @@
-const Header = () => {
+import { Menu } from "lucide-react"
+
+type Props = {
+  page: string
+}
+
+const Header = (props: Props) => {
   return (
-    <div>Header</div>
+    <div className="w-full bg-blue-800 py-4">
+      <div className="px-5 flex gap-5 items-center">
+        <button> {/* Este botão não faz nada por enquanto */}
+          <Menu className="text-neutral-50 size-9" />
+        </button>
+        <span className="text-neutral-50 font-medium text-lg">{props.page}</span>
+      </div>
+    </div>
   )
 }
 
