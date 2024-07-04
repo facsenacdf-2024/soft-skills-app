527119564cb22283f033ebf4559235b39e047d65
vnxcius <vsgh-gouveia@hotmail.com>
2024-07-04 18:22:37 -0300

    style: change max-width and rounded value for anchors

--- src/components/anchor.tsx
+++ src/components/anchor.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Anchor = (props: Props) => {
   return (
     <a href={props.link}
-      className={`bg-blue-800 hover:bg-blue-900 text-white text-sm font-medium py-1.5 px-7 rounded-full ${props.className}`}>
+      className={`bg-blue-800 hover:bg-blue-900 text-white font-semibold py-2.5 px-7 max-w-72 rounded-xl ${props.className}`}>
       <span>{props.title}</span>
     </a>
   )
