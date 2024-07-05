9f774c1b1dfc8a55130e661b60f7168d93b4ee5e
vnxcius <vsgh-gouveia@hotmail.com>
2024-07-05 16:14:41 -0300

    feat: add redo test button to results page

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -14,12 +14,13 @@ function Results() {
     <>
       <Header page="Resultados" />
 
-      <div className="max-w-xs mx-auto my-14 text-left">
+      <div className="max-w-xs sm:max-w-sm mx-auto my-14 text-left">
         <h1 className='text-center'>
           Sua pontuação foi
         </h1>
         <p className='font-bold text-center text-blue-800 text-2xl'>{VT} / 10</p>
-        <form className='my-5 space-y-5'>
+        <p className='text-center text-sm my-3 font-medium text-neutral-600'>Quanto mais próximo de 10, melhor sua inteligência emocional</p>
+        <form className='my-20 space-y-5'>
           <h1 className='leading-5'>Insira seu email para receber seu feedback</h1>
           <input type="email" name="email" id="email" placeholder='Insira seu email aqui'
             className='border border-blue-800 rounded-lg py-1.5 text-center w-full focus:outline-none placeholder:font-light' />
@@ -32,7 +33,12 @@ function Results() {
           Inteligência emocional abrange o reconhecimento e controle das emoções,
           a capacidade de automotivação, habilidades sociais e empatia. Para desenvolver...
         </p>
+        
         <Anchor title="Ver sobre" link="#" className="text-center my-10 w-full block" />
+
+        <a href="/formularios" className='text-center text-blue-500 w-fit mx-auto block'>
+          Refazer teste
+        </a>
       </div>
     </>
   )
