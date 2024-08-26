7ced01ed3b424303ad99b3fc97314c2a5d94a26f
Vini <vsgh-gouveia@hotmail.com>
2024-08-26 17:47:03 -0300

    chore: strictmode tip

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Desabilitar StrictMode para testar envio de email sem duplicar
+  // reactStrictMode: false,
+};
 
 export default nextConfig;
