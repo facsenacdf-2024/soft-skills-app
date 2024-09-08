b47ca8908310ab4e56003198e803975623b34611
Vini <vsgh-gouveia@hotmail.com>
2024-09-08 17:31:58 -0300

    chore: set tab size to 2

--- src/components/ui/Items.tsx
+++ src/components/ui/Items.tsx
@@ -3,22 +3,20 @@ import Feedback from "./feedback"
 import { Card, CardContent } from "@/components/ui/card"
 
 interface Props {
-    arr: string[],
-    title: string,
+  arr: string[],
+  title: string,
 }
 
-export default function Items({ arr, title}: Readonly<Props>) {
-
-    return (
-
-        arr.map((item: string) => (
-            <CarouselItem key={item}>
-                <Card>
-                    <CardContent>
-                        <Feedback title={title} text={item} />
-                    </CardContent>
-                </Card>
-            </CarouselItem>
-        ))
-    )
+export default function Items({ arr, title }: Readonly<Props>) {
+  return (
+    arr.map((item: string) => (
+      <CarouselItem key={item}>
+        <Card>
+          <CardContent>
+            <Feedback title={title} text={item} />
+          </CardContent>
+        </Card>
+      </CarouselItem>
+    ))
+  )
 }
