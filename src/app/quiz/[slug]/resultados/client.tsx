19c730b5e90e981cd0dfb545de70df4e0816aeb5
Jinacio23 <js.inacio746@gmail.com>
2024-09-08 21:12:52 -0300

    feat: send emails with feedbackYes

--- src/app/quiz/[slug]/resultados/client.tsx
+++ src/app/quiz/[slug]/resultados/client.tsx
@@ -76,34 +76,34 @@ export default function Client({
       //Baseado no valor de cada feedback ele retorna alguma informação
       //A princípio armazenado em options no json
       feedback.forEach((resp: number, id: any) => {
-        const questionId = quiz.questions[id].id;
-        const questionTitle = quiz.questions[id].question;
-        const questionFeedback = quiz.questions[id].feedback;
-        
-        if (resp === 1) {
-          let feed: string = quiz.questions[id].feedback;
+        let questionId = quiz.questions[id].id;
+        let questionTitle = quiz.questions[id].question;
+        let questionFeedback = quiz.questions[id].feedback;
 
-          const questionFeedback = feed;
+        //Conteúdo de feedbacks marcados com "Sim"
+        if (resp === 1) {
+          questionId = quiz.questions[id].id;
+          questionTitle = quiz.questions[id].question;
+          questionFeedback = quiz.questions[id].feedback;
+
+          //Defini os feedbacks que apareceram no carrossel
           setFeedbackYes(prevFeedbacksYes => [
             ...prevFeedbacksYes,
             questionFeedback,
           ])
+
+          //Defini conteúdo de feedbacks enviados por e-mail
+          setFeedbacks(prevFeedbacks => [
+            ...prevFeedbacks,
+            {
+              ['questionId']: questionId,
+              ['questionTitle']: questionTitle,
+              ['questionFeedback']: questionFeedback
+            },
+          ]);
+
         }
         
-        setFeedbacks(prevFeedbacks => [
-          ...prevFeedbacks,
-          {
-            ['questionId']: questionId,
-            ['questionTitle']: questionTitle,
-            ['questionFeedback']: questionFeedback
-          },
-        ]);
-
-        // Caso tenha que acessar multiplos feedbacks
-        // const questionFeedback =
-        //   resp === 1
-        //     ? quiz.questions[id].feedback[0] // Sim
-        //     : quiz.questions[id].feedback[1]; // Não
       });
     }
   }
@@ -167,7 +167,6 @@ export default function Client({
 
               </CarouselContent>
             </Carousel>
-            
             {/* Marcadores do feedback selecionado */}
             <ul className="flex justify-center gap-2 absolute bottom-2 w-full z-1">
               {
