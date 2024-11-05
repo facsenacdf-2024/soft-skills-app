de64c888295ecc2c49adf36b3ebd8a97ef12182f
vnxcius <vsgh-gouveia@hotmail.com>
2024-11-05 02:50:08 -0300

    feat: several changes to results page - Funções movidas do useEffect para uma função própria. - Adicionar textos dinâmicos ao resultado. - Exibir info dos outros estilos de liderança ao usuário. - Exibir setas de navegação do carrossel apenas em desktop. - Comentários adicionados para melhor entendimento.

--- src/app/quiz/[slug]/resultados/client.tsx
+++ src/app/quiz/[slug]/resultados/client.tsx
@@ -17,6 +17,7 @@ import Header from "@/components/header";
 import { Input } from "@/components/ui/input";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi } from "@/components/ui/carousel"
 import { Form, FormField, FormItem, FormMessage, FormControl } from "@/components/ui/form";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function Client({
   quiz,
@@ -31,8 +32,10 @@ export default function Client({
   const [liberalPercentage, setLiberalPercentage] = useState<number>(0);
   const [democratPercentage, setDemocratPercentage] = useState<number>(0);
 
-  const [api, setApi] = useState<CarouselApi>()
-  const [current, setCurrent] = useState(0)
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  const isMobile = useIsMobile();
 
   const form = useForm({
     resolver: zodResolver(sendResultsSchema),
@@ -61,17 +64,17 @@ export default function Client({
     if (localStorage.finalFeedback) {
       feedback = JSON.parse(localStorage.finalFeedback)
 
-      //Baseado no valor de cada feedback ele retorna alguma informação
-      //A princípio armazenado em options no json
+      // Baseado no valor de cada feedback ele retorna alguma informação
+      // A princípio armazenado em options no json
       feedback.forEach((resp: number, id: any) => {
 
-        //Conteúdo de feedbacks marcados com "Sim"
+        // Conteúdo de feedbacks marcados com "Sim"
         if (resp === 1) {
           const questionId = quiz.questions[id].id;
           const questionTitle = quiz.questions[id].question;
           const questionFeedback = quiz.questions[id].feedback;
 
-          //Defini conteúdo de feedbacks enviados por e-mail
+          // Define o conteúdo de feedbacks enviados por e-mail
           setFeedbacks(prevFeedbacks => [
             ...prevFeedbacks,
             {
@@ -80,47 +83,24 @@ export default function Client({
               ['questionFeedback']: questionFeedback
             },
           ]);
-
-        }
-
+        };
       });
-    }
-  }
-
-  useEffect(() => {
-    if (!api) {
-      return
-    }
-
-    setCurrent(api.selectedScrollSnap() + 1)
-
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1)
-    })
-  }, [api])
-
-  useEffect(() => {
-    setMessage('') // Limpar mensagem ao alterar email
-  }, [form.watch("email")]);
-
-  useEffect(() => {
-    const lastPoints = localStorage.getItem("lastPoints");
-    const lastResults = localStorage.getItem("lastResults");
-    if (!lastPoints && quiz.type === 1) redirect("/quiz/" + quiz?.slug + "/iniciar");
-    if (!lastResults && quiz.type === 2) redirect("/quiz/" + quiz?.slug + "/iniciar");
-
-    const parsedPoints = JSON.parse(lastPoints!);
-    const parsedResults = JSON.parse(lastResults!);
-
-    if (parsedPoints) {
-      const quizResultPoints = parsedPoints.find(
-        (result: LastPoints) => result.qID === quiz?.id
-      );
-
-      if (quizResultPoints) setPoints(quizResultPoints.lastPoints);
     };
+  };
+
+  function getLeadershipResults() {
+    // Verifica se o tipo do quiz é 2 (multiple choice)
+    // para dar continuidade na função
+    if (quiz.type !== 2) return;
+
+    const lastResults = localStorage.getItem("lastResults");
+    if (!lastResults) redirect("/quiz/" + quiz?.slug + "/iniciar");
+
+    // Transforma o plaintext armazenado no localStorage em JSON
+    const parsedResults = JSON.parse(lastResults);
 
     if (parsedResults) {
+      // Busca o quiz pelo ID armazenado no localStorage
       const quizResults = parsedResults.find(
         (result: LastResults) => result.qID === quiz?.id
       );
@@ -129,19 +109,60 @@ export default function Client({
         const autocrat = quizResults.lastResult?.autocrat || 0;
         const liberal = quizResults.lastResult?.liberal || 0;
         const democrat = quizResults.lastResult?.democrat || 0;
-        const higher = Math.max(autocrat, liberal, democrat);
 
+        // Define o estilo mais respondido
+        const higher = Math.max(autocrat, liberal, democrat);
         if (higher === autocrat) setHigherPercentage('autocrat');
         if (higher === liberal) setHigherPercentage('liberal');
         if (higher === democrat) setHigherPercentage('democrat');
 
+        // Setar os percentuais de cada estilo
         setAutocratPercentage(autocrat / quiz.questions.length * 100);
         setLiberalPercentage(liberal / quiz.questions.length * 100);
         setDemocratPercentage(democrat / quiz.questions.length * 100);
       };
     };
+  };
+
+  function getPoints() {
+    // Verifica se o tipo do quiz é 1 (sim ou não)
+    // para dar continuidade à função
+    if (quiz.type !== 1) return;
+
+    const lastPoints = localStorage.getItem("lastPoints");
+    if (!lastPoints) redirect("/quiz/" + quiz?.slug + "/iniciar");
+
+    // Transforma o plaintext armazenado no localStorage em JSON
+    const parsedPoints = JSON.parse(lastPoints);
+
+    if (parsedPoints) {
+      // Busca o quiz pelo ID armazenado no localStorage
+      const quizResultPoints = parsedPoints.find(
+        (result: LastPoints) => result.qID === quiz?.id,
+      );
+
+      if (quizResultPoints) setPoints(quizResultPoints.lastPoints);
+    };
 
     if (quiz.type === 1) recoverFeedback();
+  };
+
+  useEffect(() => {
+    if (!api) return;
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  useEffect(() => {
+    setMessage('') // Limpar mensagem ao alterar email
+  }, [form.watch("email")]);
+
+  useEffect(() => {
+    getPoints();
+    getLeadershipResults();
   }, [quiz?.id, quiz?.slug]);
 
   return (
@@ -151,7 +172,7 @@ export default function Client({
       <div className="mx-auto py-32 max-w-xs sm:max-w-4xl sm:px-5">
         <div className="mb-8">
           <h1 className="text-3xl font-bold text-blue-700 w-max ">{quiz.title}</h1>
-          <h2 className="text-2xl font-semibold text-neutral-400">Autoavalição</h2>
+          <h2 className="text-2xl font-semibold text-neutral-400">Autoavaliação</h2>
         </div>
         <div className="flex flex-col items-start">
           {quiz.type === 1 && (
@@ -299,11 +320,19 @@ export default function Client({
 
           <hr className="my-4 w-full" />
 
-          <h1 className="text-xl font-medium text-neutral-800">Feedback</h1>
+          <h1 className="text-xl font-medium text-neutral-800">
+            {quiz.type === 1 && <>Feedback</>}
+            {quiz.type === 2 && <>Saiba Mais</>}
+          </h1>
 
           <p className="text-sm text-neutral-600">
-            Essas foram as situações que você respondeu sim e que precisa
-            desenvolver melhor para fortalecer sua Inteligência Emocional
+            {quiz.type === 1 &&
+              <>
+                Essas foram as situações que você respondeu sim e que precisa
+                desenvolver melhor para fortalecer sua Inteligência Emocional
+              </>
+            }
+            {quiz.type === 2 && <>Curioso? Descubra mais sobre os outros estilos de liderança.</>}
           </p>
 
           <Carousel
@@ -314,21 +343,115 @@ export default function Client({
             opts={{ loop: true }}
             setApi={setApi}
           >
-            <CarouselContent>
-              {feedbacks.map((feedback) => (
-                <CarouselItem key={feedback.questionId}>
+            {quiz.type === 1 && (
+              <CarouselContent>
+                {feedbacks.map((feedback) => (
+                  <CarouselItem key={feedback.questionId}>
+                    <p className="font-medium mb-2">
+                      {feedback.questionId + ". " + feedback.questionTitle}
+                    </p>
+                    <hr />
+                    <p className="text-sm mt-2">
+                      {feedback.questionFeedback}
+                    </p>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            )}
+
+            {quiz.type === 2 && (
+              <CarouselContent>
+                <CarouselItem key={'autocrat'}>
                   <p className="font-medium mb-2">
-                    {feedback.questionId + ". " + feedback.questionTitle}
+                    Autocrático
                   </p>
                   <hr />
+                  <p className="text-sm my-4">
+                    Caracteriza-se por um controle centralizado e unilateral, onde
+                    o líder toma todas as decisões importantes e define as direções
+                    sem consulta ou colaboração com a equipe. Embora esse estilo
+                    possa ser eficaz em situações em que a rapidez na tomada de
+                    decisões é crucial ou onde a disciplina é necessária, ele também
+                    apresenta desvantagens significativas.
+                  </p>
+
                   <p className="text-sm mt-2">
-                    {feedback.questionFeedback}
+                    <span className="font-semibold">Pontos fortes:</span>{' '}
+                    O líder autocrático apresenta foca nas atividades, conseguindo
+                    manter a equipe concentrada nas tarefas e nos objetivos,
+                    gerando alta produtividade.
+                  </p>
+
+                  <p className="text-sm mt-2">
+                    <span className="font-semibold">Pontos fracos:</span>{' '}
+                    A falta de participação nas decisões pode gerar insatisfação entre os
+                    membros da equipe, levando a um ambiente de trabalho pouco motivador
+                    e possivelmente até à resistência passiva. Embora a produtividade possa
+                    ser alta, a qualidade dos resultados pode ser comprometida pela falta
+                    de autonomia da equipe.
                   </p>
                 </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
+                <CarouselItem key={'liberal'}>
+                  <p className="font-medium mb-2">
+                    Liberal
+                  </p>
+                  <hr />
+                  <p className="text-sm my-4">
+                    O líder liberal permite que a equipe trabalhe de forma autônoma,
+                    sem intervenção direta, confiando que os membros experientes e
+                    competentes possam tomar suas próprias decisões e se autogerenciar.
+                    Esse estilo de liderança funciona melhor com equipes qualificadas
+                    e independentes, mas pode ser inadequado onde os membros precisam
+                    de mais orientação e apoio.
+                  </p>
+
+                  <p className="text-sm mt-2">
+                    <span className="font-semibold">Pontos fortes:</span>{' '}
+                    Estimula a autonomia, criatividade e desenvolvimento de habilidades,
+                    favorecendo profissionais qualificados.
+                  </p>
+
+                  <p className="text-sm mt-2">
+                    <span className="font-semibold">Pontos fracos:</span>{' '}
+                    Pode gerar sensação de abandono e permitir que erros passem despercebidos
+                    devido à falta de supervisão.
+                  </p>
+                </CarouselItem>
+
+                <CarouselItem key={'democrat'}>
+                  <p className="font-medium mb-2">
+                    Democrático
+                  </p>
+                  <hr />
+                  <p className="text-sm my-4">
+                    O líder democrático, também conhecido como participativo, envolve
+                    o grupo nas decisões e incentiva o debate, delegando responsabilidades
+                    e aceitando opiniões divergentes. Esse estilo promove alto engajamento
+                    e qualidade nos resultados, mas pode tornar a tomada de decisões
+                    e a execução das tarefas mais lentas devido à necessidade de considerar
+                    todas as opiniões.
+                  </p>
+
+                  <p className="text-sm mt-2">
+                    <span className="font-semibold">Pontos fortes:</span>{' '}
+                    Foco nas pessoas e alto engajamento, resultando em produção de
+                    alta qualidade.
+                  </p>
+
+                  <p className="text-sm mt-2">
+                    <span className="font-semibold">Pontos fracos:</span>{' '}
+                    Pode causar lentidão nas atividades devido à necessidade de discussão
+                    e consideração de todas as ideias.
+                  </p>
+                </CarouselItem>
+              </CarouselContent>
+            )}
+            {!isMobile && (
+              <>
+                <CarouselPrevious />
+                <CarouselNext />
+              </>
+            )}
 
             {/* Marcadores do feedback selecionado */}
             <ul className="flex justify-center gap-2">
